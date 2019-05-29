9a7bcb1aa25ed9ad8e3af093815fa97f095aee6c
boonakij <boonakij@gmail.com>
2019-05-29 13:50:30 -0500

    some game functionality

--- scripts/activity.js
+++ scripts/activity.js
@@ -1,11 +1,37 @@
 $( document ).ready(function() {
+  var cardsFlipped = []
 
-  function flipCard(card) {
-    $(card).toggleClass("flipped");
+  function flipCardUp(card) {
+    $(card).addClass("flipped");
+    cardsFlipped.push(card);
+  }
+
+  function flipCardDown(card) {
+    $(card).removeClass("flipped");
+  }
+
+  function cardsMatch(cardList) {
+    // TODO: check if cardlist match
+    return false;
   }
 
   $('.card-outer').click(function() {
-    flipCard(this);
+    if (cardsFlipped.length < 2) {
+      flipCardUp(this);
+      // TODO: Probably need to add time delay here
+      setTimeout(function() {
+        if (cardsFlipped.length == 2) {
+          if (cardsMatch(cardsFlipped)) {
+            // Good condition
+          }
+          else {
+            flipCardDown(cardsFlipped[0]);
+            flipCardDown(cardsFlipped[1]);
+            cardsFlipped = []
+          }
+        }
+      }, 2500);
+    }
   });
 
 });
